ee641dab687e5fbae001e8384cd2270b4674d2b3
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-12-08 01:33:20 -0300

    Selecting form elements

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,30 @@
+function State() {
+    this.btnSave = null;
+    this.btnClear = null;
+    
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.number = null;
+    this.city = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
 export function init() {
-    const inputCep = document.forms.newAddress.cep;
-    console.log(inputCep)
+
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.city = document.forms.newAddress.city;
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]')
+    state.errorNumber = document.querySelector('[data-error="number"]')
+
+    console.log(state)
 }
