dfbad603e357a3ca1a37c624f71f1f594942e388
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-12-08 14:22:20 -0300

    test API ViaCep request

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,12 +1,12 @@
-import Address from "../models/address.js"
+import Address from "../models/address.js";
+import * as requestService from "../services/request-service.js"
 
 function State() {
-
     this.address = new Address();
 
     this.btnSave = null;
     this.btnClear = null;
-    
+
     this.inputCep = null;
     this.inputStreet = null;
     this.inputNumber = null;
@@ -19,7 +19,6 @@ function State() {
 const state = new State();
 
 export function init() {
-
     state.inputCep = document.forms.newAddress.cep;
     state.inputStreet = document.forms.newAddress.street;
     state.inputNumber = document.forms.newAddress.number;
@@ -28,31 +27,36 @@ export function init() {
     state.btnSave = document.forms.newAddress.btnSave;
     state.btnClear = document.forms.newAddress.btnClear;
 
-    state.errorCep = document.querySelector('[data-error="cep"]')
-    state.errorNumber = document.querySelector('[data-error="number"]')
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
 
-    state.inputNumber.addEventListener('change', handleInputNumberChange)
-    state.btnClear.addEventListener('click', handleBtnClearClick);
-
+    state.inputNumber.addEventListener("change", handleInputNumberChange);
+    state.btnClear.addEventListener("click", handleBtnClearClick);
+    state.btnSave.addEventListener("click", handleBtnSaveClick);
 }
 
 function handleInputNumberChange(event) {
     if (event.target.value == "") {
-        setFormerError("number", "Campo requerido!")
+        setFormerError("number", "Campo requerido!");
+    } else {
+        setFormerError("number", "");
     }
-    else {
-        setFormerError("number", "")
-    } 
 }
 
 function handleBtnClearClick(event) {
     event.preventDefault();
     clearForm();
+}
+
+async function handleBtnSaveClick(event) {
     
+    event.preventDefault()
+    const result = await requestService.getJson('https://viacep.com.br/ws/58400640/json/');
+    console.log(result);
 }
 
 function setFormerError(key, value) {
-    const element = document.querySelector(`[data-error=${key}]`)
+    const element = document.querySelector(`[data-error=${key}]`);
     element.innerHTML = value;
 }
 
